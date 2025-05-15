6eff9eea55b8add6fd07677bec9338633af62ca1
Mammadov1989 <48259421+Mammadov1989@users.noreply.github.com>
2025-05-15 15:04:55 +0400

    push

---
+++ src/Presentation/EHealthBridgeAPI.API/Extensions/GlobalExceptionMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace EHealthBridgeAPI.API.Extensions
+{
+    public static class GlobalExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGlobalExceptionMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<GlobalExceptionMiddleware>();
+        }
+    }
+}
